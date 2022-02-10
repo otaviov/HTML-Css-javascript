f544f3c18c2b645af242035a049030c1e6f2aaa3
Otavio Oliveira <71568294+otaviov@users.noreply.github.com>
2022-02-10 18:50:51 -0300

    Update teste.js

--- js/teste.js
+++ js/teste.js
@@ -5,3 +5,32 @@ for(var i = 0; i < pessoas.length; i++){
 
 }*/
 
+/*var pessoas = ["João", "José", "Maria", "Antonio", "Inacio"]; 
+
+alert(pessoas[0]);
+alert(pessoas[1]);
+alert(pessoas[4]);
+*/
+
+
+
+
+
+// EXERCÍCIOS
+
+
+// 1. Escreva um código que mostre os números ímpares entre 1 e 10
+
+/*
+for(var i = 1; i<=10; i++){
+    if(i%2 != 0){   //Se o resto for diferente de 0 é impar
+        alert(i,"ímpar");
+    }
+}
+*/
+
+// 2. Escreva um código que calcule a soma de 1 até 100. (obs: a resposta é 5050)
+
+var soma = 0;
+
+for
