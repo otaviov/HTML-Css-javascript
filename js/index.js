9efd522d27628b8b15aa405fe3b503ff2496121e
Otavio Oliveira <71568294+otaviov@users.noreply.github.com>
2022-02-12 18:58:55 -0300

    up

--- js/index.js
+++ js/index.js
@@ -23,19 +23,20 @@ function trocaBanner(){
     document.querySelector('.destaque img').src = banners[bannerAtual];
 }
 
-
 var timer = setInterval(trocaBanner, 4000);
 var controle = document.querySelector('.pause');
+
 
 controle.oneClick = function() {
     if (controle.className == 'pause') {
         clearInterval(timer);
         controle.className = 'play';
+
     } else {
-        timer = this.setInterval(trocaBanner, 4000);
+        timer = setInterval(trocaBanner, 4000);
         controle.className = 'pause';
     }
 
     return false;
-}
+};
 
