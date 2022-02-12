464a81ec4b056ecbd4191f7e508a9067a0b304a1
Otavio Oliveira <71568294+otaviov@users.noreply.github.com>
2022-02-12 18:31:23 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -12,3 +12,15 @@ var myElement = document.querySelector('#form-busca');
 myElement.onfocus = function() {
     console.log('onfocus event occurred.');
 };
+
+// Adicionando banner rotativo
+
+var banners = ["/img/destaque-home.png", "/img/destaque-home-2.png"];
+var bannerAtual = 0;
+
+function trocaBanner(){
+    bannerAtual = (bannerAtual + 1) % 2;
+    document.querySelector('.destaque img').src = banners[bannerAtual];
+}
+
+setInterval(trocaBanner, 4000);
